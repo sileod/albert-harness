65f0d4c95c3ef76c603b6573c489b39af8ee670e
sileod <damien.sileo@gmail.com>
2026-08-28 16:53:37 +0200

    release: add PyPI console entry points

---
+++ src/harness_link/entrypoints.py
@@ -0,0 +1,43 @@
+import sys
+
+from . import cli, spawn
+
+
+def harness_link():
+    cli.main()
+
+
+def harness_link_spawn():
+    spawn.main()
+
+
+def _provider(slug):
+    cli.main([slug, *sys.argv[1:]])
+
+
+def _provider_spawn(slug):
+    spawn.main([slug, *sys.argv[1:]])
+
+
+def albert():
+    _provider("albert")
+
+
+def nim():
+    _provider("nim")
+
+
+def orfree():
+    _provider("orfree")
+
+
+def albert_spawn():
+    _provider_spawn("albert")
+
+
+def nim_spawn():
+    _provider_spawn("nim")
+
+
+def orfree_spawn():
+    _provider_spawn("orfree")
